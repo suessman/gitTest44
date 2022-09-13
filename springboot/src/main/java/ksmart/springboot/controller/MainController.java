abdac28b13e22e011607104a9ff55615e03141eb
ECS <ECS@DESKTOP-048HU3O>
2022-09-13 11:32:38 +0900

    first commit

---
+++ springboot/src/main/java/ksmart/springboot/controller/MainController.java
@@ -0,0 +1,13 @@
+package ksmart.springboot.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("text", "HELLO SPRING BOOT");
+		return "main";
+	}
+}
